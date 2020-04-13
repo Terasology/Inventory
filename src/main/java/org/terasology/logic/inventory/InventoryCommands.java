a2198684a17ee3185d64b955bd02cb2e2fff0fc4
Niruandaleth <jd.rueckert@googlemail.com>
2020-04-13 23:24:57 +0200

    chore: refactor give command
    - move giveBlock commands and methods from BlockCommands to here
    - rename ItemCommands to InventoryCommands

--- src/main/java/org/terasology/logic/inventory/InventoryCommands.java
+++ src/main/java/org/terasology/logic/inventory/InventoryCommands.java
@@ -28,7 +28,6 @@ import org.terasology.entitySystem.prefab.Prefab;
 import org.terasology.entitySystem.prefab.PrefabManager;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.console.Console;
 import org.terasology.logic.console.commandSystem.annotations.Command;
 import org.terasology.logic.console.commandSystem.annotations.CommandParam;
 import org.terasology.logic.console.commandSystem.annotations.Sender;
@@ -38,14 +37,11 @@ import org.terasology.registry.In;
 import org.terasology.registry.Share;
 import org.terasology.world.block.BlockManager;
 import org.terasology.world.block.BlockUri;
-import org.terasology.world.block.entity.BlockCommands;
 import org.terasology.world.block.family.BlockFamily;
 import org.terasology.world.block.items.BlockItemComponent;
 import org.terasology.world.block.loader.BlockFamilyDefinition;
 import org.terasology.world.block.shapes.BlockShape;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
@@ -53,13 +49,10 @@ import java.util.Set;
 
 @RegisterSystem
 // TODO: Check whether this is really needed. Was introduced by darshan3 to share item commands through core registry
-@Share(ItemCommands.class)
-public class ItemCommands extends BaseComponentSystem {
+@Share(InventoryCommands.class)
+public class InventoryCommands extends BaseComponentSystem {
 
-    private static final Logger logger = LoggerFactory.getLogger(ItemCommands.class);
-
-    @In
-    private BlockCommands blockCommands;
+    private static final Logger logger = LoggerFactory.getLogger(InventoryCommands.class);
 
     @In
     private InventoryManager inventoryManager;
@@ -82,50 +75,88 @@ public class ItemCommands extends BaseComponentSystem {
             requiredPermission = PermissionManager.CHEAT_PERMISSION)
     public String give(
             @Sender EntityRef client,
-            @CommandParam("prefabId or blockName") String itemPrefabName,
-            @CommandParam(value = "amount", required = false) Integer amount,
-            @CommandParam(value = "blockShapeName", required = false) String shapeUriParam) {
+            @CommandParam("objectUri") String inventoryObjectUri,
+            @CommandParam(value = "quantity", required = false) Integer inputQuantity,
+            @CommandParam(value = "objectShapeUri", required = false) String shapeUri) {
 
-        int itemAmount = amount != null ? amount : 1;
-        if (itemAmount < 1) {
-            return "Requested zero (0) items / blocks!";
+        final Optional<Integer> requestedQuantity = Optional.ofNullable(inputQuantity);
+        if (requestedQuantity.map(q -> q < 1).orElse(false)) {
+            return "Invalid quantity of objects to remove!";
         }
 
-        Set<ResourceUrn> matches = assetManager.resolve(itemPrefabName, Prefab.class);
-
-        if (matches.size() == 1) {
-            Prefab prefab = assetManager.getAsset(matches.iterator().next(), Prefab.class).orElse(null);
-            if (prefab != null && prefab.getComponent(ItemComponent.class) != null) {
-                EntityRef playerEntity = client.getComponent(ClientComponent.class).character;
-
-                for (int quantityLeft = itemAmount; quantityLeft > 0; quantityLeft--) {
-                    EntityRef item = entityManager.create(prefab);
-                    if (!inventoryManager.giveItem(playerEntity, playerEntity, item)) {
-                        item.destroy();
-                        itemAmount -= quantityLeft;
-                        break;
-                    }
+        // assume the object to remove is a block
+        Set<ResourceUrn> matchingUrns = assetManager.resolve(inventoryObjectUri, BlockFamilyDefinition.class);
+        final Set<ResourceUrn> matchingShapeUrns = (shapeUri == null ? null : assetManager.resolve(shapeUri, BlockShape.class));
+        final boolean isBlock = !matchingUrns.isEmpty();
+        if (!isBlock) {
+            // assume the object to remove is an item
+            matchingUrns = assetManager.resolve(inventoryObjectUri, Prefab.class);
+        } else {
+            // for blocks check shapeUri if applicable
+            if (shapeUri != null) {
+                if (matchingShapeUrns.isEmpty()) {
+                    return "Could not find a shape matching \"" + shapeUri + "\"";
+                } else if (matchingShapeUrns.size() > 1) {
+                    return buildAmbiguousObjectIdString("shape", shapeUri, matchingShapeUrns);
                 }
-
-                return "You received "
-                        + (itemAmount > 1 ? itemAmount + " items of " : "an item of ")
-                        + prefab.getName() //TODO Use item display name
-                        + (shapeUriParam != null ? " (Item can not have a shape)" : "");
             }
-
-        } else if (matches.size() > 1) {
-            return buildAmbiguousObjectIdString("item", itemPrefabName, matches);
         }
 
-        // If no matches are found for items, try blocks
-        String message = blockCommands.giveBlock(client, itemPrefabName, amount, shapeUriParam);
-        if (message != null) {
-            return message;
+        // found multiple matches -> cannot remove due to ambiguity
+        if (matchingUrns.size() > 1) {
+            return buildAmbiguousObjectIdString((isBlock ? "block" : "item"), inventoryObjectUri, matchingUrns);
         }
 
-        return "Could not find an item or block matching \"" + itemPrefabName + "\"";
+        int givenItems = 0;
+        String displayName = "";
+        if (matchingUrns.size() == 1) {
+            if (isBlock) {
+                BlockFamily blockFamily = assetManager.getAsset(matchingUrns.iterator().next(), BlockFamilyDefinition.class).map(def -> {
+                    if (def.isFreeform()) {
+                        ResourceUrn shapeUrn = (matchingShapeUrns == null ? new ResourceUrn("engine:cube") : matchingShapeUrns.iterator().next());
+                        return blockManager.getBlockFamily(new BlockUri(def.getUrn(), shapeUrn));
+                    } else {
+                        return blockManager.getBlockFamily(new BlockUri(def.getUrn()));
+                    }
+                }).orElse(null);
+
+                if (blockFamily != null) {
+                    boolean isStackable = blockFamily.getArchetypeBlock().isStackable();
+                    int defaultQuantity = isStackable ? 16 : 1;
+                    int stackLimit = isStackable ? 99 : 1;
+                    int giveQuantity = (requestedQuantity.isPresent() ? requestedQuantity.get() : defaultQuantity);
+                    // TODO: check block quantity vs giveObject(giveQuantity)
+                    // TODO: use blockManager instead?
+                    EntityRef block = blockItemFactory.newInstance(blockFamily, Math.min(giveQuantity, stackLimit));
+                    displayName = blockFamily.getDisplayName();
+                    givenItems = giveObject(block, displayName, giveQuantity, client);
+                }
+            } else {
+                Prefab prefab = assetManager.getAsset(matchingUrns.iterator().next(), Prefab.class).orElse(null);
+                if (prefab != null && prefab.getComponent(ItemComponent.class) != null) {
+                    boolean isStackable = !prefab.getComponent(ItemComponent.class).stackId.isEmpty();
+                    int defaultQuantity = isStackable ? 16 : 1;
+                    int stackLimit = isStackable ? 99 : 1;
+                    int giveQuantity = (requestedQuantity.isPresent() ? requestedQuantity.get() : defaultQuantity);
+                    EntityRef item = entityManager.create(prefab);
+                    displayName = prefab.getName();
+                    givenItems = giveObject(item, displayName, giveQuantity, client);
+                }
+            }
+        }
+
+        if (givenItems > 0) {
+            return "You received "
+                    + (removedItems > 1
+                    ? removedItems + " " + (isBlock ? "blocks" : "items") + " of "
+                    : (isBlock ? "a block" : "an item") + " of ")
+                    + displayName;
+        } else {
+            return "Could not find an item or block matching \""
+                    + inventoryObjectUri
+                    + "\" in your inventory";
+        }
     }
-
 
     @Command(shortDescription = "Removes an item from your inventory",
             helpText = "Removes the desired number of the given item from your inventory",
@@ -188,7 +219,7 @@ public class ItemCommands extends BaseComponentSystem {
                 Prefab prefab = assetManager.getAsset(matchingUrns.iterator().next(), Prefab.class).orElse(null);
                 if (prefab != null && prefab.hasComponent(ItemComponent.class)) {
                     displayName = prefab.getName();
-                    removedItems = removeItem(prefab, displayName, removalQuantity, client);
+                    removedItems = removeItem(item, displayName, removalQuantity, client);
                 }
             }
         }
@@ -197,41 +228,14 @@ public class ItemCommands extends BaseComponentSystem {
         if (removedItems > 0) {
             return "You removed "
                     + (removedItems > 1
-                        ? removedItems + " " + (isBlock ? "blocks" : "items") + " of "
-                        : (isBlock ? "a block" : "an item") + " of ")
+                    ? removedItems + " " + (isBlock ? "blocks" : "items") + " of "
+                    : (isBlock ? "a block" : "an item") + " of ")
                     + displayName;
         } else {    // can also mean that all removal attempts failed
             return "Could not find an item or block matching \""
                     + inventoryObjectUri
                     + "\" in your inventory";
         }
-    }
-
-    @Command(shortDescription = "Lists all available items (prefabs)\nYou can filter by adding the beginning of words " +
-            "after the commands, e.g.: \"listItems engine: core:\" will list all items from the engine and core module",
-            requiredPermission = PermissionManager.CHEAT_PERMISSION)
-    public String listItems(@CommandParam(value = "startsWith", required = false) String[] startsWith) {
-
-        List<String> stringItems = Lists.newArrayList();
-
-        for (Prefab prefab : prefabManager.listPrefabs()) {
-            if (!BlockCommands.uriStartsWithAnyString(prefab.getName(), startsWith)) {
-                continue;
-            }
-            stringItems.add(prefab.getName());
-        }
-
-        Collections.sort(stringItems);
-
-        StringBuilder items = new StringBuilder();
-        for (String item : stringItems) {
-            if (!items.toString().isEmpty()) {
-                items.append(Console.NEW_LINE);
-            }
-            items.append(item);
-        }
-
-        return items.toString();
     }
 
     @Command(shortDescription = "Gives multiple stacks of items matching a search",
@@ -264,6 +268,32 @@ public class ItemCommands extends BaseComponentSystem {
         return result;
     }
 
+    @Command(shortDescription = "Gives multiple stacks of blocks matching a search",
+            helpText = "Adds all blocks that match the search parameter into your inventory",
+            runOnServer = true, requiredPermission = PermissionManager.CHEAT_PERMISSION)
+    public String bulkGiveBlock(
+            @Sender EntityRef sender,
+            @CommandParam("searched") String searched,
+            @CommandParam(value = "quantity", required = false) Integer quantityParam,
+            @CommandParam(value = "shapeName", required = false) String shapeUriParam) {
+
+        if (quantityParam != null && quantityParam < 1) {
+            return "Here, have these zero (0) blocks just like you wanted";
+        }
+
+        String searchLowercase = searched.toLowerCase();
+        List<String> blocks = findBlockMatches(searchLowercase);
+        String result = "Found " + blocks.size() + " block matches when searching for '" + searched + "'.";
+        if (blocks.size() > 0) {
+            result += "\nBlocks:";
+            for (String block : blocks) {
+                result += "\n" + block + "\n";
+                result += giveBlock(sender, block, quantityParam, shapeUriParam);
+            }
+        }
+        return result;
+    }
+
     private String buildAmbiguousObjectIdString(String objectType, String objectUri, Set<ResourceUrn> possibleMatches) {
         StringBuilder builder = new StringBuilder();
         builder.append("Specified ");
@@ -274,6 +304,16 @@ public class ItemCommands extends BaseComponentSystem {
         Joiner.on(", ").appendTo(builder, possibleMatches);
 
         return builder.toString();
+    }
+
+    private int giveObject(EntityRef object, String displayName, final int removalQuantity, EntityRef client) {
+        EntityRef playerEntity = client.getComponent(ClientComponent.class).character;
+        for (int quantityLeft = removalQuantity; quantityLeft > 0; quantityLeft--) {
+            if (!inventoryManager.giveItem(playerEntity, playerEntity, object)) {
+                object.destroy();
+                return quantityLeft;
+            }
+        }
     }
 
     private int removeObjectFromSlot(EntityRef slot, EntityRef playerEntity, boolean isStackable, String displayName, int quantity) {
@@ -352,5 +392,12 @@ public class ItemCommands extends BaseComponentSystem {
         }
 
         return removalQuantity - quantityLeft;
+    }
+
+    private List<String> findBlockMatches(String searchLowercase) {
+        return assetManager.getAvailableAssets(BlockFamilyDefinition.class)
+                .stream().<Optional<BlockFamilyDefinition>>map(urn -> assetManager.getAsset(urn, BlockFamilyDefinition.class))
+                .filter(def -> def.isPresent() && def.get().isLoadable() && matchesSearch(searchLowercase, def.get()))
+                .map(r -> new BlockUri(r.get().getUrn()).toString()).collect(Collectors.toList());
     }
 }
