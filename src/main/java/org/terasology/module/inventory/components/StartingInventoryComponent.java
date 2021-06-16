b27cf312886ef6f80db4c6a6d3e0fff444fa4cab
Harshit <52852877+ahv15@users.noreply.github.com>
2021-06-17 03:19:03 +0530

    feat: add RequestInventoryEvent (#44)
    - add `RequestInventoryEvent` trigger event to fill an entity's inventory with the given items
    
    - refactor `StartingInventorySystem` to use this event

--- src/main/java/org/terasology/module/inventory/components/StartingInventoryComponent.java
+++ src/main/java/org/terasology/module/inventory/components/StartingInventoryComponent.java
@@ -4,8 +4,6 @@ package org.terasology.module.inventory.components;
 
 import com.google.common.collect.Lists;
 import org.terasology.engine.entitySystem.Component;
-import org.terasology.engine.entitySystem.prefab.Prefab;
-import org.terasology.engine.world.block.Block;
 
 import java.util.List;
 
@@ -46,31 +44,4 @@ public class StartingInventoryComponent implements Component {
      * Default is an empty list.
      */
     public List<InventoryItem> items = Lists.newLinkedList();
-
-    /**
-     * A simple class connecting a resource (a {@link Block} or {@link Prefab}) to a quantity
-     */
-    public static class InventoryItem {
-        /**
-         * A resource uri, may be either a block uri or an item uri.
-         */
-        public String uri;
-
-        /**
-         * Must be greater than 0.
-         * <p>
-         * Default value is 1.
-         */
-        public int quantity = 1;
-
-        /**
-         * A list of objects to be nested inside this inventory item.
-         * <p>
-         * Adding inventory items to this list will cause a {@link InventoryComponent} to be added to this object. The
-         * nested inventory is filled with the items specified in this list.
-         * <p>
-         * Default value is the empty list.
-         */
-        public List<InventoryItem> items = Lists.newLinkedList();
-    }
 }
---
+++ src/main/java/org/terasology/module/inventory/events/RequestInventoryEvent.java
@@ -0,0 +1,17 @@
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+package org.terasology.module.inventory.events;
+
+import org.terasology.engine.entitySystem.event.Event;
+import org.terasology.module.inventory.components.InventoryItem;
+
+import java.util.List;
+/**
+ * This event is sent to fill the inventory of a player with the requested items.
+ */
+public class RequestInventoryEvent implements Event {
+    public List<InventoryItem> items;
+    public RequestInventoryEvent(List<InventoryItem> items) {
+        this.items = items;
+    }
+}
