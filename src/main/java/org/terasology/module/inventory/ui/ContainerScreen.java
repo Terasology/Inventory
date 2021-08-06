5e8a0e5c0d12065385c9dc8072d46774dfa647d5
jdrueckert <jd.rueckert@googlemail.com>
2021-08-06 21:12:25 +0200

    feat: add inventory screen titles (#45)
    This adds a (translated) title to the inventory screen.
    
    In itself this doesn't do much yet and might seem unnecessary. However, especially when using item blocks that have an inventory (chests, assembly tables, etc.), it easily becomes confusing, which inventory is the own and which is the inventory of the block being interacted with. Hence, I added this for container inventory as well.
    
    If the container parent prefab does not have a `DisplayNameComponent`, the prefab name will be shown, for instance `CoreAdvancedAssets:chest`.
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/module/inventory/ui/ContainerScreen.java
+++ src/main/java/org/terasology/module/inventory/ui/ContainerScreen.java
@@ -3,11 +3,15 @@
 package org.terasology.module.inventory.ui;
 
 import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.prefab.Prefab;
+import org.terasology.engine.i18n.TranslationSystem;
 import org.terasology.engine.logic.characters.CharacterComponent;
+import org.terasology.engine.logic.common.DisplayNameComponent;
 import org.terasology.engine.logic.players.LocalPlayer;
-import org.terasology.nui.databinding.ReadOnlyBinding;
 import org.terasology.engine.registry.In;
 import org.terasology.engine.rendering.nui.CoreScreenLayer;
+import org.terasology.nui.databinding.ReadOnlyBinding;
+import org.terasology.nui.widgets.UILabel;
 
 /**
  */
@@ -15,6 +19,9 @@ public class ContainerScreen extends CoreScreenLayer {
 
     @In
     private LocalPlayer localPlayer;
+
+    @In
+    private TranslationSystem i18n;
 
     private InventoryGrid containerInventory;
 
@@ -29,13 +36,29 @@ public class ContainerScreen extends CoreScreenLayer {
         });
         inventory.setCellOffset(10);
 
+        UILabel containerTitle = find("containerTitle", UILabel.class);
         containerInventory = find("container", InventoryGrid.class);
+
+        EntityRef characterEntity = localPlayer.getCharacterEntity();
         containerInventory.bindTargetEntity(new ReadOnlyBinding<EntityRef>() {
             @Override
             public EntityRef get() {
-                EntityRef characterEntity = localPlayer.getCharacterEntity();
-                CharacterComponent characterComponent = characterEntity.getComponent(CharacterComponent.class);
-                return characterComponent.predictedInteractionTarget;
+                return characterEntity.getComponent(CharacterComponent.class).predictedInteractionTarget;
+            }
+        });
+        containerTitle.bindText(new ReadOnlyBinding<String>() {
+            @Override
+            public String get() {
+                Prefab parentPrefab = characterEntity.getComponent(CharacterComponent.class).predictedInteractionTarget.getParentPrefab();
+                DisplayNameComponent displayName = parentPrefab.getComponent(DisplayNameComponent.class);
+                if (displayName != null) {
+                    // The display name may contain a translatable string reference, thus we attempt to get the translation.
+                    // If the string is just non-translatable display name the fallback mechanism will yield just the input string.
+                    // NOTE: Unfortunately, this contract is not guaranteed by `TranslationSystem#translate(String)`.
+                    return i18n.translate(displayName.name);
+                } else {
+                    return parentPrefab.getName();
+                }
             }
         });
     }
